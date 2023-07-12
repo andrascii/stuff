93e7e85c5aeb86d1c5591d879b72731df11c0adf
Andrey <46888555+andrascii@users.noreply.github.com>
2023-07-12 21:32:12 +0300

    added build script for OS Windows release build

---
+++ vs2019-release-generate.bat
@@ -0,0 +1,7 @@
+SET BUILDDIR=build-release
+mkdir %BUILDDIR%
+cd %BUILDDIR%
+del CMakeCache.txt
+cd ..
+cmake -B %BUILDDIR% -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%VCPKG_CMAKE_FILE% -DBUILD_TESTS=OFF
+pause
