4feb1111831eb8a9d97ef8c30d200f7dfe14d9aa
Andrey <46888555+andrascii@users.noreply.github.com>
2023-09-27 02:31:58 +0300

    added tcp-server/tcp-client empty projects

---
+++ projects/tcp-client/stdafx.h
@@ -0,0 +1,44 @@
+#pragma once
+
+//
+// C/C++
+//
+#include <atomic>
+#include <cassert>
+#include <chrono>
+#include <condition_variable>
+#include <csignal>
+#include <cstdlib>
+#include <deque>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <queue>
+#include <set>
+#include <string>
+#include <thread>
+#include <type_traits>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include <filesystem>
+
+
+//
+// Expected
+//
+#include <tl/expected.hpp>
+
+//
+// nlohmann
+//
+#include <nlohmann/json.hpp>
+
+using namespace std::chrono_literals;
+using namespace std::string_literals;
